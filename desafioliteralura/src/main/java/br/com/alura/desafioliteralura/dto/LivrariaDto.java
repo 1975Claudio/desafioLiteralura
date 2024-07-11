5ee328388db035347bf1c4f5ba491c14b7c62a90
CLAUDIO_ALMEIDA <157855960+BackEndInnovation@users.noreply.github.com>
2024-07-11 16:43:31 -0300

    segunda atualização

--- desafioliteralura/src/main/java/br/com/alura/desafioliteralura/dto/LivrariaDto.java
+++ desafioliteralura/src/main/java/br/com/alura/desafioliteralura/dto/LivrariaDto.java
@@ -2,7 +2,7 @@ package br.com.alura.desafioliteralura.dto;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-gitimport java.util.List;
+import java.util.List;
 
 @JsonIgnoreProperties (ignoreUnknown = true)
 public record LivrariaDto(@JsonAlias("results") List<LivroDto> livros) {
